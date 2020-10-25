07059d098cc1f842e6266e7f7f6c3b466578ecaf
Farhan Bin Amin <farhanbinamin@gmail.com>
2020-10-25 20:21:45 +0600

    added custom font & color in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray': '#E5E0DA',
+        'custom-black': '#232323',
+        'custom-blue': '#1644DA'
+      },
+      fontFamily: {
+        primary: ['Heebo'],
+        secondary: ['Space Mono']
+      }
+    },
   },
   variants: {},
   plugins: [],
