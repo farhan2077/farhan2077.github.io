fa401dab62e5c81198bda7826d3725de8db5564c
Farhan Bin Amin <farhanbinamin@gmail.com>
2020-10-26 11:48:39 +0600

    added landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,14 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      screens: {
+        'custom-screen': '850px',
+      },
       colors: {
         'custom-gray': '#E5E0DA',
         'custom-black': '#232323',
-        'custom-blue': '#1644DA'
+        'custom-blue': '#1644DA',
+        'custom-yellow': '#FFFF00'
       },
       fontFamily: {
         primary: ['Heebo'],
