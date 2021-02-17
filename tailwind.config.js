b4bf09d51c07bb5133645901be650f5bff6cf9e5
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-18 00:48:35 +0600

    feat: upgraded to tailwind_v2 & using postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,27 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: {
     enabled: true,
-    content: ['index.html'],
-},
+    content: ["index.html"],
+  },
   theme: {
     extend: {
       margin: {
-        '22': '5.5rem',
-        '46': '11.5rem',
-        '-14': '-3.5rem',
+        22: "5.5rem",
+        46: "11.5rem",
+        "-14": "-3.5rem",
       },
       colors: {
-        'custom-gray': '#E5E0DA',
-        'custom-black': '#232323',
-        'custom-blue': '#1644DA',
-        'custom-yellow': '#FFFF00',
+        "custom-gray": "#E5E0DA",
+        "custom-black": "#232323",
+        "custom-blue": "#1644DA",
+        "custom-yellow": "#FFFF00",
       },
       fontFamily: {
-        primary: ['Heebo'],
-        secondary: ['Space Mono'],
-      }
+        primary: ["Heebo"],
+        secondary: ["Space Mono"],
+      },
     },
   },
   variants: {},
   plugins: [],
-}
+};
