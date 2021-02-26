76e24c797a4cecb2ada5147f03897f901fcf3c9d
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-27 01:00:32 +0600

    new: initial file structure + css container setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Home from "./pages/Home"
+
 function App() {
   return (
-    <div>
-      <p className="text-green-500">react w/ tailwindcss is working</p>
-    </div>
+    <>
+    <Home />
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       fontFamily: {
         primary: ["Inter"],
