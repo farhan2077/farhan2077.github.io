e41ab038ef644082f31b45bcfec0aeda03f1143e
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-27 16:34:20 +0600

    fix: fixed styling issue in intoduction.js

--- src/partials/Introduction.js
+++ src/partials/Introduction.js
@@ -3,16 +3,16 @@ import React from "react";
 export default function Introduction() {
   return (
     <>
-      <main className="-mt-16 h-screen">
+      <main className="-mt-32 md:-mt-16 h-screen">
         <div className="flex items-center h-full">
           <div className="font-primary">
             <p className="text-gray-600 text-xl">👋 there!</p>
-            <h1 className="my-20 text-gray-600 font-bold text-7xl">
-              I am <span className="text-blue-600">Farhan Bin Amin</span>.<br/>I love creating beautiful<br/>stuff for the web.
+            <h1 className="my-16 md:my-20 text-gray-600 font-bold text-5xl md:text-7xl">
+              I am&nbsp;<br className="visible md:hidden" /><span className="text-blue-600">Farhan Bin Amin</span>.<br/>I love creating beautiful<br/>stuff for the web.
             </h1>
             <div>
               <button className="btn btn-blue">view my projects</button>
-              <button className="btn btn-gray ml-12">get in touch</button>
+              <button className="btn btn-gray ml-0 sm:ml-12 mt-3 md:mt-0">get in touch</button>
             </div>
           </div>
         </div>
