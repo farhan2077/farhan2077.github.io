1381c14f7a07c213818a528a8c29ce6ff73c9ec5
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-27 15:17:10 +0600

    fix: fixed navbar hard coded links

--- src/partials/Navbar.js
+++ src/partials/Navbar.js
@@ -7,7 +7,7 @@ const navItems = [
   {
     id: 1,
     navItem: "about",
-    navItemUrl: "/#bottom",
+    navItemUrl: "/#about",
   },
   {
     id: 2,
@@ -31,25 +31,22 @@ export default function Navbar() {
       <nav className="invisible md:visible">
         <div className="flex flex-col md:flex-row font-secondary text-md justify-between mt-5 items-center">
           <img
-            className="h-auto md:h-6 w-8 bg-gray-100 md:w-auto "
+            className="h-auto md:h-6 w-8 bg-gray-100 md:w-auto"
             src={logoImage}
             alt="logo"
           />
-          <div className="flex gap-x-20 items-center text-gray-500">
+          <div className="flex gap-x-20 items-center text-gray-500 capitalize">
             {navItems.map((item) =>
               item.navItem !== "resume" ? (
-                <HashLink
-                  key={item.id}
-                  smooth
-                  to="#bottom"
-                  className=""
-                >
+                <HashLink key={item.id} smooth to={item.navItemUrl}>
                   {item.navItem}
                 </HashLink>
               ) : (
                 <a
                   key={item.id}
                   href="https://farhan2077.github.io/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="border-2 border-gray-400 rounded px-4 py-2 shadow-sm"
                 >
                   {item.navItem}
