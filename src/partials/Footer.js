26d87ddc6fc4f96bcdddcd128116d3242b05fe56
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-01 01:29:27 +0600

    new: added emojis

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import rockon from "../images/emojis/rockon.png";
 
 export default function Footer() {
   return (
@@ -7,7 +8,15 @@ export default function Footer() {
         <a href="https://github.com/farhan2077">
           Designed & built by
           <br />
-          Farhan
+          <div className="capitalize">
+            <div className="text-center">
+              <span className="mr-0.5">Farhan Bin Amin</span>
+              <span>
+                <img src={rockon} alt="hech yeah!" className="inline w-5 h-5"/>
+              </span>
+              <span className="ml-0.5">2021</span>
+            </div>
+          </div>
         </a>
       </p>
     </>
--- src/partials/Introduction.js
+++ src/partials/Introduction.js
@@ -1,4 +1,5 @@
 import React from "react";
+import wave from "../images/emojis/wave.png"
 
 export default function Introduction() {
   return (
@@ -6,7 +7,9 @@ export default function Introduction() {
       <main className="-mt-32 md:-mt-16 h-screen">
         <div className="flex items-center h-full">
           <div className="font-primary">
-            <p className="text-gray-600 text-xl">👋 there!</p>
+            <p className="text-gray-600 text-xl">
+            <img src={wave} alt="Hi" className="inline w-6 mr-2 mb-1"/>
+            there!</p>
             <h1 className="my-16 md:my-20 text-gray-600 font-bold text-5xl md:text-7xl tracking-tight">
               I am&nbsp;<br className="visible md:hidden" /><span className="text-blue-600">Farhan Bin Amin</span>.<br/>I love creating beautiful<br/>stuff for the web.
             </h1>
