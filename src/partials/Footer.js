2b773bdc9a6d7a1a962f414771aacec0f09a4de5
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-01 11:36:01 +0600

    fix: tightened up footer texts

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -4,15 +4,19 @@ import rockon from "../images/emojis/rockon.png";
 export default function Footer() {
   return (
     <>
-      <p className="font-secondary text-gray-500 text-xs md:text-sm leading-normal text-center mb-2 mt-0 md:-mt-2">
-        <a href="https://github.com/farhan2077">
+      <p className="font-secondary text-gray-500 text-xs md:text-sm leading-normal text-center mb-2 mt-0 md:-mt-2 tracking-tight">
+        <a
+          href="https://github.com/farhan2077"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Designed & built by
           <br />
           <div className="capitalize">
             <div className="text-center">
               <span className="mr-0.5">Farhan Bin Amin</span>
               <span>
-                <img src={rockon} alt="hech yeah!" className="inline w-5 h-5"/>
+                <img src={rockon} alt="hech yeah!" className="inline w-5 h-5" />
               </span>
               <span className="ml-0.5">2021</span>
             </div>
