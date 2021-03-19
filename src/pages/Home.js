1b02c8bffe72f7892efca9c5e5e3641e56c18f43
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-19 12:24:11 +0600

    new: added noading animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../partials/navbar/Navbar";
 import Introduction from "../partials/Introduction";
 import About from "../partials/About";
@@ -6,30 +6,48 @@ import Skills from "../partials/skills/Skills";
 import FeaturedProjects from "../partials/projects/featured-projects/FeaturedProjects";
 import GetNotified from "../partials/GetNotified";
 import Contact from "../partials/Contact";
-import Footer from "../partials/Footer"
+import Footer from "../partials/Footer";
+
+import logo from "../images/logo/logo250.png";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1650);
+  }, []);
+
   return (
     <>
-      {/* container start */}
-      <div className="md:container px-4 sm:px-10 lg:px-20 xl:px-52">
-        <div>
-          <Navbar />
-          <Introduction />
-          <About />
-          <div className="m-28"></div>
-          <Skills />
-          <div className="m-28"></div>
-          <FeaturedProjects />
-          <div className="m-28"></div>
-          <GetNotified />
-          <div className="m-28"></div>
-          <Contact />
-          <div className="m-16"></div>
-          <Footer />
+      {isLoading ? (
+        <div className="flex flex-1 justify-center items-center h-screen fadeout">
+          <img
+            src={logo}
+            alt="site_logo"
+            className="h-10 w-auto animate-bounce mb-10 sm:mb-0"
+          />
         </div>
-      </div>
-      {/* container end */}
+      ) : (
+        <div className="md:container px-4 sm:px-10 lg:px-20 xl:px-52">
+          <div className="fadein">
+            <Navbar />
+            <Introduction />
+            <About />
+            <div className="m-28"></div>
+            <Skills />
+            <div className="m-28"></div>
+            <FeaturedProjects />
+            <div className="m-28"></div>
+            <GetNotified />
+            <div className="m-28"></div>
+            <Contact />
+            <div className="m-16"></div>
+            <Footer />
+          </div>
+        </div>
+      )}
     </>
   );
 }
