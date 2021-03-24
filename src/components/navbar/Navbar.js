4fcba2bbb5c574661ecfa3fe55d5849105d50675
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-24 12:58:48 +0600

    new: added resume

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -113,6 +113,7 @@ export default function Navbar() {
                         target="_blank"
                         rel="noopener noreferrer"
                         className="border-2 border-gray-400 rounded px-4 py-1.5 shadow-sm text-gray-500 hover:text-gray-700 hover:border-gray-500 tracking-tight"
+                        download
                       >
                         {item.navItem}
                       </a>
