322ea5f8fbc464da32bbf96882e63f1f1af4eff1
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-04-10 17:55:43 +0600

    fix: removed loading animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import BackToTop from "components/BackToTop";
 import Navbar from "components/navbar/Navbar";
@@ -9,47 +9,25 @@ import FeaturedProjects from "components/projects/featured-projects/FeaturedProj
 import Contact from "components/Contact";
 import Footer from "components/Footer";
 
-import logo from "assets/logos/logo.png";
-
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1650);
-  }, []);
-
   return (
     <>
-      {isLoading ? (
-        <div className="flex flex-1 justify-center items-center h-screen fadeoutDelay">
-          <img
-            src={logo}
-            alt="site_logo"
-            className="h-10 w-auto animate-bounce mb-10 sm:mb-0"
-          />
-        </div>
-      ) : (
+      <BackToTop />
+      <Navbar />
+      <div className="md:container px-4 sm:px-10 lg:px-20 xl:px-52">
         <div>
-          <BackToTop />
-          <Navbar />
-          <div className="md:container px-4 sm:px-10 lg:px-20 xl:px-52">
-            <div className="fadein">
-              <Introduction />
-              <About />
-              <div className="m-28"></div>
-              <Skills />
-              <div className="m-28"></div>
-              <FeaturedProjects />
-              <div className="m-32"></div>
-              <Contact />
-              <div className="m-16"></div>
-              <Footer />
-            </div>
-          </div>
+          <Introduction />
+          <About />
+          <div className="m-28"></div>
+          <Skills />
+          <div className="m-28"></div>
+          <FeaturedProjects />
+          <div className="m-32"></div>
+          <Contact />
+          <div className="m-16"></div>
+          <Footer />
         </div>
-      )}
+      </div>
     </>
   );
 }
