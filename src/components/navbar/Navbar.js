6d9b668a941b8300982b3d1fba0fa042f6e5e802
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-04-20 01:18:34 +0600

    refactor: nav menu resume UI

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -112,7 +112,7 @@ export default function Navbar() {
                         href={
                           process.env.PUBLIC_URL + "/resume_farhan-bin-amin.pdf"
                         }
-                        className="border-2 border-gray-400 rounded px-4 py-1.5 shadow-sm text-gray-500 hover:text-gray-700 hover:border-gray-500 tracking-tight"
+                        className="capitalize text-blue-500 hover:text-blue-700 tracking-tight"
                       >
                         {item.navItem}
                       </a>
