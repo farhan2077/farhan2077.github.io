9aed8de1237599865a529bd9ab4017f5261584dd
Farhan Bin Amin <farhanbinamin@gmail.com>
2022-01-07 22:56:51 +0600

    chore: updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,16 @@
 import React from "react";
-import rockon from "assets/emojis/rockon.png";
 
 export default function Footer() {
   return (
     <div>
-      <p className="font-secondary text-gray-500 text-xs md:text-sm leading-normal text-center mb-2 mt-0 md:-mt-2 tracking-tight">
+      <p className="mt-0 mb-2 text-xs leading-normal tracking-tight text-center text-gray-500 font-secondary md:text-sm md:-mt-2">
         <a
           href="https://github.com/farhan2077"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Designed & built by
+          Designed & built by Farhan
           <br />
-          <div className="capitalize">
-            <div className="text-center">
-              <span className="mr-0.5">Farhan Bin Amin</span>
-              <span>
-                <img src={rockon} alt="hech yeah!" className="inline w-5 h-5" />
-              </span>
-              <span className="ml-0.5">2021</span>
-            </div>
-          </div>
         </a>
       </p>
     </div>
