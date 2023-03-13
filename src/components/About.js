5db086383b054a013c83546eef600f484e1ba482
farhanbinamin@gmail.com <farhanbinamin@gmail.com>
2023-03-14 02:07:28 +0600

    new: updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,9 @@ export default function About() {
       <span id="about"></span>
       <h2 className="header">about</h2>
       <section className="grid mt-6 ml-0 mr-0 md:mt-10 md:ml-28 md:mr-40 gap-y-4 text-primary">
+        <p>Hi! I’m from Khulna, Bangladesh.</p>
         <p>
-          Hello! I’m Farhan from Khulna.
-          <br />I have completed my graduation in
+          I have completed my graduation in
           <span className="font-semibold">
             &nbsp;Electronics & Communication Engineering&nbsp;
           </span>
@@ -29,8 +29,17 @@ export default function About() {
           also beautiful bringing elegant experience.
         </p>
         <p>
-          Whenever I am not studying or working, I’m probably watching movies or
-          tv-shows.
+          I love taking pictures and sometimes upload them to&nbsp;
+          <a
+            className="text-link underline-anim"
+            href="https://www.instagram.com/farhan.bin.amin_/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            instagram
+          </a>
+          . Also, whenever I am not studying or working, I’m probably listening
+          to The Weeknd or watching movies or tv-shows.
         </p>
       </section>
     </div>
